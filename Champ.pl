2d32e6a0d32d835830566ceac4663dcf29b590d3
Karim Alaoui <karimalaoui@acces1183.res.insa-lyon.fr>
2013-10-14 20:04:41 +0200

    Updating the main

--- Champ.pl
+++ Champ.pl
@@ -2,6 +2,6 @@
 1 si gagné par joueur 1, 2 si gagné par 2, 3 si plein et non gagné'
 V est la liste des grilles gagnantes*/
 
-etatChamp(W,V) :- etatGrilleChamp(0,A1,_),etatGrilleChamp(1,B2,_),etatGrilleChamp(2,C3,_),etatGrilleChamp(3,D4,_),
-etatGrilleChamp(4,E5,_),etatGrilleChamp(5,F6,_),etatGrilleChamp(6,G7,_),etatGrilleChamp(7,H8,_), etatGrilleChamp(8,I9,_),
+etatChamp(W,V) :- etatGrilleChamp(1,A1,_),etatGrilleChamp(2,B2,_),etatGrilleChamp(3,C3,_),etatGrilleChamp(4,D4,_),
+etatGrilleChamp(5,E5,_),etatGrilleChamp(6,F6,_),etatGrilleChamp(7,G7,_),etatGrilleChamp(8,H8,_), etatGrilleChamp(9,I9,_),
 gagnee(A1,B2,C3,D4,E5,F6,G7,H8,I9,V,W),!.
--- main.pl
+++ main.pl
@@ -1,5 +1,6 @@
 :- ['GrilleSolo.pl'].
 :- ['Champ.pl'].
+:- ['input.pl'].
 
 /*Miscellaneous functions*/
 
@@ -19,27 +20,29 @@ imprimerGS :- gridsState(G), print(G).
 /*****************/
 /*INITIALIZATION*/
 /*Game field*/
-:- asserta(champJeu([[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]])).
+
 /*Grid state (0=normal, 1=won by player 1, 2=won by player 2, 3=full)*/
-:- asserta(gridsState([0,0,0,0,0,0,0,0,0])).
-:- asserta(dernierCoup(-1,-1,-1)).
+/*:- asserta(gridsState([0,0,0,0,0,0,0,0,0])).*/
+/*:- asserta(dernierCoup(-1,-1,-1)).*/
 
 /*****************/
 /* MAIN FUNCTIONS*/
 
-allowedGrids(L) :- dernierCoup(_,M,_), gridsState(D), nth1(M,D,X), X = 0, append([],[M],L). /*can play on normal grid*/
-allowedGrids(L) :- dernierCoup(_,M,_), gridsState(D), nth1(M,D,X), X > 0, zeroesPos(D,L). /*can play on normal grid*/
-allowedGrids(L) :- dernierCoup(-1,-1,_), append([],[1,2,3,4,5,6,7,8,9],L).
+allowedGrids(L,State) :- dernierCoup(_,M,_), nth1(M,State,X), X = 0, append([],[M],L). /*can play on normal grid*/
+allowedGrids(L,State) :- dernierCoup(_,M,_), nth1(M,State,X), X > 0, zeroesPos(State,L). /*can play on normal grid*/
+allowedGrids(L,_State) :- dernierCoup(-1,-1,_), append([],[1,2,3,4,5,6,7,8,9],L). /*first time, one can play anywhere*/
 
 nextPlayer(X) :- dernierCoup(_,_,-1), X is 1.
 nextPlayer(X) :- dernierCoup(_,_,P), P > 0, X is 3-P.
 
-jouerCoup(N,M,J) :- champJeu(D), nth1(N,D,X), nth1(M,X,Y), Y \= 1, Y \= 2, /*checks that location not already played*/
-					nextPlayer(J), /*check that the player is the good one */
-					allowedGrids(Al), member(N,Al), /*checks that the grid is allowed*/
-					replace(X,M,J,NewX),replace(D,N,NewX,NewD), retract(champJeu(D)), assert(champJeu(NewD)), /*updates grid*/
-					dernierCoup(A,B,C), retract(dernierCoup(A,B,C)), asserta(dernierCoup(N,M,J)), /*updates dernierCoup*/
-					etatGrilleChamp(N,E,_), gridsState(G), replace(G,N,E,NewGridsState), retract(gridsState(G)), assert(gridsState(NewGridsState)),
-					print('Le prochain coup doit etre joue en '), allowedGrids(L), print(L), print(', par le joueur '), nextPlayer(NP), print(NP).
+getGridsState(X) :- etatGrilleChamp(1,X1,_),etatGrilleChamp(2,X2,_),etatGrilleChamp(3,X3,_),etatGrilleChamp(4,X4,_),etatGrilleChamp(5,X5,_),etatGrilleChamp(6,X6,_),etatGrilleChamp(7,X7,_),etatGrilleChamp(8,X8,_),etatGrilleChamp(9,X9,_), append([],[X1,X2,X3,X4,X5,X6,X7,X8,X9],X). /*a remplacer par la fonction de Champ quand elle marchera.*/
+
+/*jouerCoup(N,M,J) :- champJeu(D), nth1(N,D,X), nth1(M,X,Y), Y \= 1, Y \= 2, *//*checks that location not already played*/
+/*					nextPlayer(J), *//*check that the player is the good one */
+/*					allowedGrids(Al), member(N,Al),*/ /*checks that the grid is allowed*/
+/*					replace(X,M,J,NewX),replace(D,N,NewX,NewD), retract(champJeu(D)), assert(champJeu(NewD)), *//*updates grid*/
+/*					dernierCoup(A,B,C), retract(dernierCoup(A,B,C)), asserta(dernierCoup(N,M,J)), */ /*updates dernierCoup*/
+/*					etatGrilleChamp(N,E,_), gridsState(G), replace(G,N,E,NewGridsState), retract(gridsState(G)), assert(gridsState(NewGridsState)),
+					print('Le prochain coup doit etre joue en '), allowedGrids(L), print(L), print(', par le joueur '), nextPlayer(NP), print(NP). */
+
+:- getGridsState(X), allowedGrids(Al,X), print(X), print(','), print(Al).
