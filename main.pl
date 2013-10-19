88fba7f0ae23d658ccaf7e1e119d90584d27f27b
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2013-10-19 15:06:45 +0200

    Cleaned the code a bit. Now working.

--- main.pl
+++ main.pl
@@ -1,7 +1,6 @@
 :- ['GrilleSolo.pl'].
 :- ['Champ.pl'].
 :- ['input.pl'].
-:- ['IA_0.1.pl'].
 
 /*Miscellaneous functions*/
 
@@ -19,41 +18,36 @@ printLastMove :- lastMove(X,Y,Z), print(X), print(','), print(Y), print(', joueu
 printGridState :- gridsState(G), print(G).
 
 /*****************/
-/*INITIALIZATION*/
-/*Game field*/
-
-/*Grid state (0=normal, 1=won by player 1, 2=won by player 2, 3=full)*/
-/*:- asserta(gridsState([0,0,0,0,0,0,0,0,0])).*/
-/*:- asserta(lastMove(-1,-1,-1)).*/
-
-/* IA functions */
-playableGrid(N) :- getGridsState(State), allowedGrids(Al,State), nth0(0,Al,N).
-/*Called with N, and I=1, to fill M*/
-playableCell(N, M) :- playableGrid(N), gameField(D), nth1(N, D, G), nth1(M, G, 0).
-
-/*****************/
 /* MAIN FUNCTIONS*/
 
-allowedGrids([M],State) :- lastMove(_,M,_), nth1(M,State,0). /*can play on normal grid*/
-allowedGrids(L,State) :- lastMove(_,M,_), nth1(M,State,X), X > 0, zeroesPos(State,L). /*can play on normal grid*/
-allowedGrids([1,2,3,4,5,6,7,8,9],_) :- lastMove(-1,-1,_). /*first time, one can play anywhere*/
+/* Normal case: play on the grid at the last cell's position */
+allowedGrids([M],State) :- lastMove(_,M,_), nth1(M, State, 0). 
+/* If the grid on the last cell's position have already be won, play on any valid grid */
+allowedGrids(L,State) :- lastMove(_,M,_), nth1(M, State, X), X > 0, zeroesPos(State, L).
+/* Default initialization value: one can play anywhere */
+allowedGrids([1,2,3,4,5,6,7,8,9],_) :- lastMove(-1,-1,_). 
 
+/* Returns the state of all grids as a list. For instance: [0, 1, 2, 0, 0, 2, 1, 0, 0]*/
+getGridsState([X1,X2,X3,X4,X5,X6,X7,X8,X9]) :- fieldState(1,X1,_),fieldState(2,X2,_),fieldState(3,X3,_),fieldState(4,X4,_),fieldState(5,X5,_),fieldState(6,X6,_),fieldState(7,X7,_),fieldState(8,X8,_),fieldState(9,X9,_).
+
+/* Functions to make dealing with playable cells/grids easier */
+playableGrid(N) :- getGridsState(State), allowedGrids(Allowed,State), member(N, Allowed).
+playableCell(N, M) :- playableGrid(N), gameField(D), nth1(N, D, G), nth1(M, G, 0).
 
 nextPlayer(X) :- lastMove(_,_,-1), X is 1.
 nextPlayer(X) :- lastMove(_,_,P), P > 0, X is 3-P.
 
-getGridsState(X) :- fieldState(1,X1,_),fieldState(2,X2,_),fieldState(3,X3,_),fieldState(4,X4,_),fieldState(5,X5,_),fieldState(6,X6,_),fieldState(7,X7,_),fieldState(8,X8,_),fieldState(9,X9,_), append([],[X1,X2,X3,X4,X5,X6,X7,X8,X9],X). /*a remplacer par la fonction de Champ quand elle marchera.*/
 
-playMove(N,M,J) :- gameField(D), nth1(N,D,X), nth1(M,X,Y), Y \= 1, Y \= 2, /*checks that location not already played*/
+playMove(N,M,J) :- gameField(D), nth1(N,D,G), nth1(M,G,0), /*checks that location not already played*/
 					nextPlayer(J), /*check that the player is the good one */
 					getGridsState(State), allowedGrids(Al,State), member(N,Al), /*checks that the grid is allowed*/
-					replace(X,M,J,NewX),replace(D,N,NewX,NewD), retract(gameField(D)), assert(gameField(NewD)), /*updates grid*/
+					replace(G,M,J,NewG),replace(D,N,NewG,NewD), retract(gameField(D)), assert(gameField(NewD)), /*updates grid*/
 					lastMove(A,B,C), retract(lastMove(A,B,C)), asserta(lastMove(N,M,J)).  /*updates lastMove*/
-					/*print('Le pro    in coup doit etre joue en '), allowedGrids(L), print(L), print(', par le joueur '), nextPlayer(NP), print(NP).*/ 
-
 
 /********************/
 /* EXECUTION BLOCK */
 
-:- playableCell(N,M), nextPlayer(J), nextMove(N,M,J), !, playMove(N,M,J). /*the AI plays*/
-:- gameField(Ch), getGridsState(X), allowedGrids(Al,X), print(Ch), print(' '), print(Al), print(' '), print(X). /*the result is sent*/
+/* The AI plays its move*/
+:- playableCell(N,M), nextPlayer(J), nextMove(N,M,J), !, playMove(N,M,J). 
+/* Prints the result on screen */
+:- gameField(Ch), getGridsState(X), allowedGrids(Al,X), print(Ch), print(' '), print(Al), print(' '), print(X). 
