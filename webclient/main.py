fc8e08b967a1e98aca001f9da35b8aada5cecfd2
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2013-10-19 13:15:34 +0200

    Many changes to the webclient. Still needs to be tested with an AI.

--- webclient/main.py
+++ webclient/main.py
@@ -1,13 +1,54 @@
 from flask import Flask, render_template, request
 import json
-
+from subprocess import Popen, PIPE, STDOUT
 
 # Initializing the app
 app = Flask(__name__)
 
+def serialize_state(board, grid_n, cell_n, last_player):
+    ser = str()
+    ser += ":- ['ai1.pl'].\n"
+    ser += ':- asserta(gameField({})).\n'.format(json.dumps(board))
+    ser += ':- asserta(lastMove({}, {}, {})).\n'.format(grid_n, cell_n, last_player)
+
+    return ser
+
+def ai_call(board, grid_n, cell_n, last_player):
+    with open('input.pl', 'w') as input_file:
+        input_file.write(serialize_state(board, grid_n, cell_n, last_player))
+
+    p = Popen(['prolog', '-q', '-s', '../main.pl'], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    output = p.communicate()
+    print "OUTPUT = ", output[0].strip()
+    output_tokens = output[0].strip().split(' ')
+    board = json.loads(output_tokens[0])
+    playable_grids = json.loads(output_tokens[1])
+    state = json.loads(output_tokens[2])
+    return dict(board=board, playable=playable_grids, state=state)
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/play", methods=["POST"])
+def play():
+    data = json.loads(request.data)
+    print data
+    # Applying the current player's move
+    player = int(data['player'])
+    print 'GOT PLAYER'
+
+    grid = int(data['grid'])
+    cell = int(data['cell'])
+    board = data['board']
+    board[grid - 1][cell - 1] = player
+
+    # Calling the AI for the next move
+    resp = ai_call(board, grid, cell, last_player=player)
+    print resp
+    
+    return json.dumps(resp)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- webclient/static/main.js
+++ webclient/static/main.js
@@ -1,15 +1,15 @@
 // Initialization
 SYMBOL = {1: 'X', 2: 'O', 0: '&nbsp;'}
+PLAYABLE = [6]
+BOARD = [ [1, 1, 0, 0, 0, 2, 0, 0, 0], [1, 0, 1, 0, 0, 0, 2, 2, 2], [1, 0, 0, 2, 0, 0, 1, 0, 2],
+          [1, 0, 2, 1, 0, 2, 1, 2, 0], [0, 2, 0, 0, 1, 0, 2, 2, 0], [1, 1, 0, 0, 0, 0, 0, 0, 0],
+          [1, 1, 0, 0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 2, 0, 2, 0, 0], [1, 1, 0, 0, 0, 0, 0, 0, 0] ];
+STATE = [0, 1, 0, 2, 0, 0, 0, 0, 0];
 
-$(document).ready(function() {
+// TODO : Require AI to return the last move to update this
+LAST_MOVE = {grid:1, cell:6, player:2};
 
-    PLAYABLE = [6]
-    BOARD= [ [1, 1, 0, 0, 0, 2, 0, 0, 0], [1, 0, 1, 0, 0, 0, 2, 2, 2], [1, 0, 0, 2, 0, 0, 1, 0, 2],
-             [1, 0, 2, 1, 0, 2, 1, 2, 0], [0, 2, 0, 0, 1, 0, 2, 2, 0], [1, 1, 0, 0, 0, 0, 0, 0, 0],
-             [1, 1, 0, 0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 2, 0, 2, 0, 0], [1, 1, 0, 0, 0, 0, 0, 0, 0] ];
-
-    LAST_MOVE = {grid:1, cell:6, player:2};
-
+updateGame = function() {
     next_player = LAST_MOVE.player;
     current_player = 3 - next_player;
 
@@ -34,6 +34,56 @@ $(document).ready(function() {
 
     // Marking-up the last move
     $('.gamefield > ul:nth-child('+(LAST_MOVE.grid)+') > li:nth-child('+(LAST_MOVE.cell)+')').attr('class', 'last-move');
+} 
+
+$(document).ready(function() {
+    updateGame();
+
+    //Adding callbacks
+    $('.gamefield > ul > li').click(function(event) {
+        var cell = $(event.target);
+        var grid = cell.parent();
+
+        if (grid.attr('class') != 'playable' || BOARD[grid.index()][cell.index()] != 0) {
+            return;
+        }
+
+        // Updating the grid with the move and displaying it
+        LAST_MOVE.player = 3 - LAST_MOVE.player;
+        LAST_MOVE.grid = grid.index() + 1;
+        LAST_MOVE.cell = cell.index() + 1;
+        BOARD[grid.index()][cell.index()] = LAST_MOVE.player; 
+
+        updateGame();
+
+
+
+        console.log('Sending : ');
+        console.log({
+            board: BOARD,
+            player: current_player,
+            grid:grid.index() + 1,
+            cell:cell.index() + 1
+          });
+
+
+        $.ajax({
+            url: '/play',
+            method: 'POST',
+            data: {
+                board: BOARD,
+                player: current_player,
+                grid:grid.index() + 1,
+                cell:cell.index() + 1
+            },
+            success : function (data) {
+                console.log('RECEIVED : ');
+                console.log(data);
+            }
+
+        });
+        
+    }); // End of cell-click event
 
  
 });
