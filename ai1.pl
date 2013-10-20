1dc6a83ef9272053f674a92e317ac4ee31d28b27
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2013-10-20 21:56:12 +0200

    Added a couple more predicates to the AI.
    We should switch to a comprehensive "weights" system to simplify adding other criterias/priorities to the AI.

--- ai1.pl
+++ ai1.pl
@@ -1,31 +1,47 @@
 % On évite d'aller sur une grille qu'on va perdre
 avoidLoss(M,J) :- NextJ is 3 - J, not(isWinningMove(M, _, NextJ)).
+spoilWin(N, M, J) :- NextJ is 3 - J, isWinningMove(N, M, NextJ).
 % On essaie de jouer une case pour gagner la grille actuelle
 winGrid(N,M,J) :- isWinningMove(N, M, J).
 
-% Change this later (the same as "weights")
-cellWeight(M, W) :- W is 3 - (2 - M mod 3) - (1 - M // 3).
+weight(M, 1) :- member(M, [1,3,7,9]).
+weight(M, 2) :- member(M, [2,4,6,8]).
+weight(5, 3).
+
+% Avoid loss or going on a grid that have been won (== giving all choices to the next player) AND winning
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(N, 0, _), winGrid(N, M, J), weight(M, 1).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(N, 0, _), winGrid(N, M, J), weight(M, 2).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(N, 0, _), winGrid(N, M, J), weight(M, 3).
+
+% Avoid loss or going on a grid that have been won (== giving all choices to the next player)
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(N, 0, _), weight(M, 1).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(N, 0, _), weight(M, 2).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(N, 0, _), weight(M, 3).
 
 % Avoid loss and win the current grid
-nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), winGrid(N, M, J), cellWeight(M, 1).
-nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), winGrid(N, M, J), cellWeight(M, 2).
-nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), winGrid(N, M, J), cellWeight(M, 3).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), winGrid(N, M, J), weight(M, 1).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), winGrid(N, M, J), weight(M, 2).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), winGrid(N, M, J), weight(M, 3).
+
+% Avoid loss and spoil a future win from the other player
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), spoilWin(N, M, J), weight(M, 1).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), spoilWin(N, M, J), weight(M, 2).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), spoilWin(N, M, J), weight(M, 3).
 
 % Avoid loss
-nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), cellWeight(M, 1).
-nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), cellWeight(M, 2).
-nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), cellWeight(M, 3).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), weight(M, 1).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), weight(M, 2).
+nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), weight(M, 3).
 
 % Win the current grid
-nextMove(N, M, J) :- playableCell(N,M), winGrid(N, M, J), cellWeight(M, 1).
-nextMove(N, M, J) :- playableCell(N,M), winGrid(N, M, J), cellWeight(M, 2).
-nextMove(N, M, J) :- playableCell(N,M), winGrid(N, M, J), cellWeight(M, 3).
-
+nextMove(N, M, J) :- playableCell(N,M), winGrid(N, M, J), weight(M, 1).
+nextMove(N, M, J) :- playableCell(N,M), winGrid(N, M, J), weight(M, 2).
+nextMove(N, M, J) :- playableCell(N,M), winGrid(N, M, J), weight(M, 3).
 
 % Play the best cell
-nextMove(N, M, _) :- playableCell(N,M), cellWeight(M, 1).
-nextMove(N, M, _) :- playableCell(N,M), cellWeight(M, 2).
-nextMove(N, M, _) :- playableCell(N,M), cellWeight(M, 3).
+nextMove(N, M, _) :- playableCell(N,M), weight(M, 1).
+nextMove(N, M, _) :- playableCell(N,M), weight(M, 2).
+nextMove(N, M, _) :- playableCell(N,M), weight(M, 3).
 
 
 % bestMove(_,M,J) :- write('#1 N = '), write(N), write(' M = '), write(M), NextJ is 3 - J, not(vaGagner(M, _, NextJ)).
