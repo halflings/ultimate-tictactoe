6f718358abe67a17b50a8a5f376a556ce1dedb62
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2013-10-25 11:20:21 +0200

    Fixed bug in "avoidWon" predicate (must fix fieldState)

--- ai1.pl
+++ ai1.pl
@@ -2,64 +2,6 @@
 avoidLoss(M, J) :- NextJ is 3 - J, not(isWinningMove(M, _, NextJ)).
 spoilWin(N, M, J) :- NextJ is 3 - J, isWinningMove(N, M, NextJ).
 
-weight(M, 1) :- member(M, [1,3,7,9]).
-weight(M, 2) :- member(M, [2,4,6,8]).
-weight(5, 3).
-
-% TODO : integrate the "dynamic weights" (see below)
-
-%% % Avoid loss
-%% % Avoid going on a grid that have been won
-%% % Win the current grid
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(M, 0, _), isWinningMove(N, M, J), weight(M, 1).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(M, 0, _), isWinningMove(N, M, J), weight(M, 2).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(M, 0, _), isWinningMove(N, M, J), weight(M, 3).
-
-%% % Avoid loss
-%% % Avoid going on a grid that have been won
-%% % Spoil a win
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(M, 0, _), spoilWin(N, M, J), weight(M, 1).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(M, 0, _), spoilWin(N, M, J), weight(M, 2).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(M, 0, _), spoilWin(N, M, J), weight(M, 3).
-
-%% % Avoid loss
-%% % Avoid going on a grid that have been won
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(M, 0, _), weight(M, 1).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(M, 0, _), weight(M, 2).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), fieldState(M, 0, _), weight(M, 3).
-
-%% % Avoid going on a grid that have been won
-%% nextMove(N, M, _) :- playableCell(N,M), fieldState(M, 0, _), weight(M, 1).
-%% nextMove(N, M, _) :- playableCell(N,M), fieldState(M, 0, _), weight(M, 2).
-%% nextMove(N, M, _) :- playableCell(N,M), fieldState(M, 0, _), weight(M, 3).
-
-%% % Avoid loss
-%% % Win the current grid
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), isWinningMove(N, M, J), weight(M, 1).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), isWinningMove(N, M, J), weight(M, 2).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), isWinningMove(N, M, J), weight(M, 3).
-
-%% % Avoid loss 
-%% % Spoil a future win from the other player
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), spoilWin(N, M, J), weight(M, 1).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), spoilWin(N, M, J), weight(M, 2).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), spoilWin(N, M, J), weight(M, 3).
-
-%% % Avoid loss
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), weight(M, 1).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), weight(M, 2).
-%% nextMove(N, M, J) :- playableCell(N,M), avoidLoss(M, J), weight(M, 3).
-
-%% % Win the current grid
-%% nextMove(N, M, J) :- playableCell(N,M), isWinningMove(N, M, J), weight(M, 1).
-%% nextMove(N, M, J) :- playableCell(N,M), isWinningMove(N, M, J), weight(M, 2).
-%% nextMove(N, M, J) :- playableCell(N,M), isWinningMove(N, M, J), weight(M, 3).
-
-%% % Play the best cell
-%% nextMove(N, M, _) :- playableCell(N,M), weight(M, 1).
-%% nextMove(N, M, _) :- playableCell(N,M), weight(M, 2).
-%% nextMove(N, M, _) :- playableCell(N,M), weight(M, 3).
-
 
 
 % Priorities:
@@ -72,20 +14,25 @@ weight(5, 3).
 % 7. Play a "good" grid (based on its 'weight') (weight up to 3)
 
 nextMove(N, M, J) :- bestCell(N, M, J, _), !.
-bestCell(N, M, J, W) :- playableCell(N, M), cellWeight(N, M, J, W), not(biggerWeight(J, W)).
+bestCell(N, M, J, W) :- playableCell(N, M), cellWeight(N, M, J, W), not(biggerWeight(J, W)), !.
 biggerWeight(J, W) :- playableCell(XN, XM), cellWeight(XN, XM, J, XW), XW > W.
 
-cellWeight(N, M, J, W) :- weight(M, W1), winWeight(N, M, J, W2), spoilWeight(N, M, J, W3), avoidWinWeight(N, M, J, W4),
+cellWeight(N, M, J, W) :- baseWeight(M, W1), winWeight(N, M, J, W2), spoilWinWeight(N, M, J, W3), avoidWinWeight(N, M, J, W4),
                           avoidWonWeight(N, M, J, W5), W is W1 + W2 + W3 + W4 + W5.
+
+baseWeight(M, 1) :- member(M, [1,3,7,9]).
+baseWeight(M, 2) :- member(M, [2,4,6,8]).
+baseWeight(5, 3).
 
 winWeight(N, M, J, 8) :- isWinningMove(N, M, J), !.
 winWeight(_, _, _, 0).
 
-spoilWeight(N, M, J, 16) :- NextJ is 3 - J, isWinningMove(N, M, NextJ), !.
-spoilWeight(_, _, _, 0).
+spoilWinWeight(N, M, J, 16) :- NextJ is 3 - J, isWinningMove(N, M, NextJ), !.
+spoilWinWeight(_, _, _, 0).
 
 avoidWinWeight(_, M, J, 32) :- NextJ is 3 - J, not(isWinningMove(M, _, NextJ)), !.
 avoidWinWeight(_, _, _, 0).
 
-avoidWonWeight(_, M, _, 64) :- fieldState(M, 0, _), !.
+% TODO : Fix fieldState
+avoidWonWeight(_, M, _, 64) :- fieldState(M, W, _), W = 0, !.
 avoidWonWeight(_, _, _, 0).
