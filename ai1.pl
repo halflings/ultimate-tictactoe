a5c89eb536eb702e28b766a1bbd8a49f5d7ce3ac
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2013-10-27 00:46:00 +0200

    Added another criteria: avoid going on a grid where you could win

--- ai1.pl
+++ ai1.pl
@@ -10,29 +10,37 @@ spoilWin(N, M, J) :- NextJ is 3 - J, isWinningMove(N, M, NextJ).
 % 3. Avoid going on a grid where the next player can win in one move (weight: 32) 
 % 4. Spoil a win in the current grid (weight: 3 + 4 + 8 + 1 = 16)
 % 5. Win the current grid (weight: 3 + 4 + 1 = 8)
-% 6. (NOT IMPLEMENTED) Go on a grid where the other player never played (weight: 4)
-% 7. Play a "good" grid (based on its 'weight') (weight up to 3)
+% 6. Avoid going on a grid where you need one extra move to win
+% 7. Go on a grid where the other player never played (weight: 4)
+% 8. Play a "good" grid (based on its 'weight') (weight up to 3)
 
 nextMove(N, M, J) :- bestCell(N, M, J, _), !.
 bestCell(N, M, J, W) :- playableCell(N, M), cellWeight(N, M, J, W), not(biggerWeight(J, W)), !.
 biggerWeight(J, W) :- playableCell(XN, XM), cellWeight(XN, XM, J, XW), XW > W.
 
-cellWeight(N, M, J, W) :- baseWeight(M, W1), winWeight(N, M, J, W2), spoilWinWeight(N, M, J, W3), avoidWinWeight(N, M, J, W4),
-                          avoidWonWeight(N, M, J, W5), W is W1 + W2 + W3 + W4 + W5.
+cellWeight(N, M, J, W) :- baseWeight(M, W1), emptyGridWeight(M, J, W2), avoidSpoiledWin(N, J, W3), winWeight(N, M, J, W4),
+    spoilWinWeight(N, M, J, W5), avoidWinWeight(N, M, J, W6), avoidWonWeight(N, M, J, W7),
+  	W is W1 + W2 + W3 + W4 + W5 + W6 + W7, !.
 
 baseWeight(M, 1) :- member(M, [1,3,7,9]).
 baseWeight(M, 2) :- member(M, [2,4,6,8]).
 baseWeight(5, 3).
 
-winWeight(N, M, J, 8) :- isWinningMove(N, M, J), !.
+emptyGridWeight(N, J, 4) :- NextJ is 3 - J, gameField(F), nth1(N, F, G), count(G, NextJ, 0).
+emptyGridWeight(_, _, 0).
+
+avoidSpoiledWin(N, J, 8) :- not(isWinningMove(N, _, J)). 
+avoidSpoiledWin(_, _, 0).
+
+winWeight(N, M, J, 16) :- isWinningMove(N, M, J).
 winWeight(_, _, _, 0).
 
-spoilWinWeight(N, M, J, 16) :- NextJ is 3 - J, isWinningMove(N, M, NextJ), !.
+spoilWinWeight(N, M, J, 32) :- NextJ is 3 - J, isWinningMove(N, M, NextJ).
 spoilWinWeight(_, _, _, 0).
 
-avoidWinWeight(_, M, J, 32) :- NextJ is 3 - J, not(isWinningMove(M, _, NextJ)), !.
+avoidWinWeight(_, M, J, 64) :- NextJ is 3 - J, not(isWinningMove(M, _, NextJ)).
 avoidWinWeight(_, _, _, 0).
 
 % TODO : Fix fieldState
-avoidWonWeight(_, M, _, 64) :- fieldState(M, W, _), W = 0, !.
+avoidWonWeight(_, M, _, 128) :- fieldState(M, W, _), W = 0.
 avoidWonWeight(_, _, _, 0).
