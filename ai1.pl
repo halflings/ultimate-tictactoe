d73b6884b32f32303558fd211ba57e7eca8cf185
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2013-10-31 19:48:06 +0100

    Fixed bug with spoiled wins (thanks Valentina!), should work better.

--- ai1.pl
+++ ai1.pl
@@ -18,9 +18,9 @@ nextMove(N, M, J) :- bestCell(N, M, J, _), !.
 bestCell(N, M, J, W) :- playableCell(N, M), cellWeight(N, M, J, W), not(biggerWeight(J, W)), !.
 biggerWeight(J, W) :- playableCell(XN, XM), cellWeight(XN, XM, J, XW), XW > W.
 
-cellWeight(N, M, J, W) :- baseWeight(M, W1), emptyGridWeight(M, J, W2), avoidSpoiledWin(N, J, W3), winWeight(N, M, J, W4),
-    spoilWinWeight(N, M, J, W5), avoidWinWeight(N, M, J, W6), avoidWonWeight(N, M, J, W7),
-  	W is W1 + W2 + W3 + W4 + W5 + W6 + W7, !.
+cellWeight(N, M, J, W) :- baseWeight(M, W8), emptyGridWeight(M, J, W7), avoidSpoiledWinWeight(M, J, W6),
+    winWeight(N, M, J, W5), spoilWinWeight(N, M, J, W4), avoidWinWeight(N, M, J, W3), avoidWonWeight(N, M, J, W2),
+  	W is W2 + W3 + W4 + W5 + W6 + W7 + W8, !.
 
 baseWeight(M, 1) :- member(M, [1,3,7,9]).
 baseWeight(M, 2) :- member(M, [2,4,6,8]).
@@ -29,8 +29,8 @@ baseWeight(5, 3).
 emptyGridWeight(N, J, 4) :- NextJ is 3 - J, gameField(F), nth1(N, F, G), count(G, NextJ, 0).
 emptyGridWeight(_, _, 0).
 
-avoidSpoiledWin(N, J, 8) :- not(isWinningMove(N, _, J)). 
-avoidSpoiledWin(_, _, 0).
+avoidSpoiledWinWeight(N, J, 8) :- not(isWinningMove(N, _, J)). 
+avoidSpoiledWinWeight(_, _, 0).
 
 winWeight(N, M, J, 16) :- isWinningMove(N, M, J).
 winWeight(_, _, _, 0).
